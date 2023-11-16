f8fe5186e7f1e25d1f1bd9a177d1a14c938e3c78
jasonh7702 <127449610+jasonh7702@users.noreply.github.com>
2023-11-16 01:37:53 -0500

    updating 3-0, 3-1, 2-0 counts

--- MLBresearch.R
+++ MLBresearch.R
@@ -3,10 +3,10 @@ library(tidyverse)
 library(Lahman)
 
 data2022 %>% 
-       mutate(RUNS =  AWAY_SCORE_CT + HOME_SCORE_CT,
-               HALF.INNING = paste(GAME_ID, INN_CT, BAT_HOME_ID),
-               RUNS.SCORED = (BAT_DEST_ID > 3) + (RUN1_DEST_ID > 3) +
-              (RUN2_DEST_ID > 3) + (RUN3_DEST_ID > 3)) -> data2022
+  mutate(RUNS =  AWAY_SCORE_CT + HOME_SCORE_CT,
+         HALF.INNING = paste(GAME_ID, INN_CT, BAT_HOME_ID),
+         RUNS.SCORED = (BAT_DEST_ID > 3) + (RUN1_DEST_ID > 3) +
+           (RUN2_DEST_ID > 3) + (RUN3_DEST_ID > 3)) -> data2022
 data2022 %>% 
   group_by(HALF.INNING) %>% 
   summarize(Outs.Inning = sum(EVENT_OUTS_CT),
@@ -19,9 +19,9 @@ data2022 %>%  inner_join(half_innings, by = "HALF.INNING") %>%
 
 data2022 %>% 
   mutate(BASES = paste(ifelse(BASE1_RUN_ID > '', 1, 0),
-                        ifelse(BASE2_RUN_ID > '', 1, 0),
+                       ifelse(BASE2_RUN_ID > '', 1, 0),
                        ifelse(BASE3_RUN_ID > '', 1, 0), sep = ""),
-                       STATE = paste(BASES, OUTS_CT)) -> data2022
+         STATE = paste(BASES, OUTS_CT)) -> data2022
 
 data2022 %>% 
   mutate(NRUNNER1 = 
@@ -87,7 +87,7 @@ data2022 <- data2022 %>%
 data2022 <- data2022 %>% 
   mutate(c12 = grepl(paste0("^", b, s, s,
                             "|", s, b, s,
-                            "l", s, s, "[FR]*", b), sequence),
+                            "|", s, s, "[FR]*", b), sequence),
          c22 = grepl(paste0("^",b,b,s,s,
                             "|",b,s,b,s,
                             "|",b,s,s,"[FR]*",b,
@@ -97,8 +97,8 @@ data2022 <- data2022 %>%
                      sequence),
          c32 = grepl(paste0("^", s, "*", b, s,
                             "*", b, s, "*", b), sequence)
-                     & grepl(paste0("^",b,"*",s,b,"*",s),
-                             sequence))
+         & grepl(paste0("^",b,"*",s,b,"*",s),
+                 sequence))
 data2022 %>% select(RUNS.ROI, STATE, c10) %>% 
   group_by(STATE, c10) %>%  summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> runc10
 
@@ -112,6 +112,10 @@ data2022 %>% filter(c20 == TRUE) %>%
   summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> c10_c20_c30
 
 data2022 %>% filter(c30 == TRUE) %>% 
+  group_by(STATE, c31) %>%  
+  summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> c10_c20_c30_c31
+
+data2022 %>% filter(c31 == TRUE) %>% 
   group_by(STATE, c31) %>%  
   summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> c10_c20_c30_c31
 
@@ -151,7 +155,7 @@ transitions <- list(runc10 = runc10, c10_c20 = c10_c20, c10_c20_c30 =
                     c01_c11=c01_c11,c01_c02=c01_c02,c01_c11_c12=c01_c11_c12)
 
 runc10 %>% pivot_wider(values_from = rmean, names_from  = c10, names_prefix = "c10_") %>% 
-mutate(diff = c10_TRUE - c10_FALSE) 
+  mutate(diff = c10_TRUE - c10_FALSE) 
 
 data2022 %>% select(GAME_ID, EVENT_ID, Runs.Inning, c00, c10, c20,
                     c11, c01, c30, c21, c31, c02, c12, c22, c32) -> new.data2022
@@ -265,7 +269,7 @@ count_plot1_22
 data2022 %>% select(RUNS.ROI, STATE, c10) %>% 
   filter(c10 == TRUE) %>% 
   mutate(outs = as.numeric(sub("(.+)\\s(\\d)$", "\\2", STATE))) %>%
-  mutate(STATE = sub("\\s\\d$", "", STATE))%>% 
+  mutate(STATE = sub("\\s\\d$", "", STATE)) %>% 
   group_by(STATE,outs, c10) %>%  
   summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> runc10filter
 
@@ -287,4 +291,221 @@ count_plot12_ <- runc10filter %>%
   labs(title = "10 Pitch Count")
 count_plot12_
 
-save(transitions,file="transitions.RData")
+
+#creating a 0-0 count 
+data2022 %>% filter(c01 == FALSE | c02 == FALSE | c10 == FALSE |
+                      c20 == FALSE | c30 == FALSE | c11 == FALSE
+                    | c21 == FALSE | c31 == FALSE | c32 == FALSE | c22 == FALSE
+                    | c12 == FALSE) %>%
+  mutate(outs = as.numeric(sub("(.+)\\s(\\d)$", "\\2", STATE))) %>%
+  mutate(STATE = sub("\\s\\d$", "", STATE)) %>% 
+  group_by(STATE, outs, c00) %>%  
+  summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> c00
+
+data2022 %>% filter(c01 == FALSE | c02 == FALSE | c10 == FALSE |
+                      c20 == FALSE | c30 == FALSE | c11 == FALSE
+                    | c21 == FALSE | c31 == FALSE | c32 == FALSE | c22 == FALSE
+                    | c12 == FALSE) %>% 
+  mutate(outs = as.numeric(sub("(.+)\\s(\\d)$", "\\2", STATE))) %>%
+  mutate(STATE = sub("\\s\\d$", "", STATE)) %>% 
+  group_by(STATE, outs, c00) %>%  
+  summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> c00filtered
+
+
+#plot for 00 plots
+count_plot00 <- c00 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rpos)) +
+  geom_tile() +
+  geom_text(aes(label = round(rpos, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "00 Pitch Count")
+count_plot00
+
+count_plot002 <- c00 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rmean)) +
+  geom_tile() +
+  geom_text(aes(label = round(rmean, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "00 Pitch Count")
+count_plot002
+
+
+data2022 %>% filter(c20 == TRUE, c30 == TRUE) %>% 
+  mutate(outs = as.numeric(sub("(.+)\\s(\\d)$", "\\2", STATE))) %>%
+  mutate(STATE = sub("\\s\\d$", "", STATE)) %>% 
+  group_by(STATE, outs, c30) %>%  
+  summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> c10_c20_c30filter
+
+data2022 %>% filter(c31 == TRUE) %>% 
+  mutate(outs = as.numeric(sub("(.+)\\s(\\d)$", "\\2", STATE))) %>%
+  mutate(STATE = sub("\\s\\d$", "", STATE)) %>% 
+  group_by(STATE, outs, c31) %>%  
+  summarize(rmean = mean(RUNS.ROI), rpos = mean(RUNS.ROI>0)) -> c10_c20_c30_c31filter
+
+count_plot30 <- c10_c20_c30filter %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rpos)) +
+  geom_tile() +
+  geom_text(aes(label = round(rpos, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "30 Pitch Count")
+count_plot30
+
+count_plot302 <- c10_c20_c30filter %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rmean)) +
+  geom_tile() +
+  geom_text(aes(label = round(rmean, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "30 Pitch Count")
+count_plot302 
+
+
+
+#Load the patchwork package
+library(patchwork)
+
+#Combine the plots into a 2x2 grid
+combined_plots <- count_plot00 + count_plot002 + count_plot30 + count_plot302
+
+#Arrange the plots as a 2x2 grid
+combined_plots <- combined_plots + plot_layout(ncol = 2)
+
+combined_plots
+
+print(c01, n=100)
+
+#combining the 0-0 and 3-1 data sets
+combined_data31 <- bind_rows(c10_c20_c30_c31filter, c00filtered)
+
+#calculating the differences between data sets
+combined_data31 <- combined_data31 %>%
+  mutate(rmean_diff = lag(rmean, default = first(rmean)) - rmean,
+         rpos_diff = lag(rpos, default = first(rpos)) - rpos) %>% 
+  select(STATE, outs, rmean_diff, rpos_diff) %>%
+  slice(25:n())
+
+combined_data30 <- bind_rows(c10_c20_c30filter, c00filtered)
+combined_data30 <- combined_data30 %>%
+  mutate(rmean_diff = lag(rmean, default = first(rmean)) - rmean,
+         rpos_diff = lag(rpos, default = first(rpos)) - rpos) %>% 
+  select(STATE, outs, rmean_diff, rpos_diff) %>%
+  slice(25:n())
+
+combined_data20 <- bind_rows(c10_c20filter, c00filtered)
+combined_data20 <- combined_data20 %>%
+  mutate(rmean_diff = lag(rmean, default = first(rmean)) - rmean,
+         rpos_diff = lag(rpos, default = first(rpos)) - rpos) %>% 
+  select(STATE, outs, rmean_diff, rpos_diff) %>%
+  slice(25:n())
+
+combined_data10 <- bind_rows(runc10filter, c00filtered)
+combined_data10 <- combined_data10 %>%
+  mutate(rmean_diff = lag(rmean, default = first(rmean)) - rmean,
+         rpos_diff = lag(rpos, default = first(rpos)) - rpos) %>% 
+  select(STATE, outs, rmean_diff, rpos_diff) %>%
+  slice(25:n())
+
+combined_data31rmean <- combined_data31 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rmean_diff)) +
+  geom_tile() +
+  geom_text(aes(label = round(rmean_diff, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "31 Pitch Count")
+combined_data31rmean 
+
+combined_data31rpos <- combined_data31 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rpos_diff)) +
+  geom_tile() +
+  geom_text(aes(label = round(rpos_diff, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "31 Pitch Count")
+combined_data31rpos 
+
+combined_data30rmean <- combined_data30 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rmean_diff)) +
+  geom_tile() +
+  geom_text(aes(label = round(rmean_diff, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "30 Pitch Count")
+combined_data30rmean 
+
+combined_data30rpos <- combined_data30 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rpos_diff)) +
+  geom_tile() +
+  geom_text(aes(label = round(rpos_diff, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "30 Pitch Count")
+combined_data30rpos 
+
+combined_data20rmean <- combined_data20 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rmean_diff)) +
+  geom_tile() +
+  geom_text(aes(label = round(rmean_diff, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "20 Pitch Count")
+combined_data20rmean 
+
+combined_data20rpos <- combined_data20 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rpos_diff)) +
+  geom_tile() +
+  geom_text(aes(label = round(rpos_diff, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "20 Pitch Count")
+combined_data20rpos 
+
+combined_data10rmean <- combined_data10 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rmean_diff)) +
+  geom_tile() +
+  geom_text(aes(label = round(rmean_diff, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "10 Pitch Count")
+combined_data10rmean 
+
+combined_data10rpos <- combined_data10 %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rpos_diff)) +
+  geom_tile() +
+  geom_text(aes(label = round(rpos_diff, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "10 Pitch Count")
+combined_data10rpos 
+
+#combining all counts and 0-0 counts into a single grid
+library(gridExtra)
+grid.arrange(combined_data31rmean, combined_data31rpos,
+             combined_data30rmean, combined_data30rpos,
+             combined_data20rmean, combined_data20rpos,
+             combined_data10rmean, combined_data10rpos, nrow = 4)
+
+count_plot31 <- c10_c20_c30_c31filter %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rmean)) +
+  geom_tile() +
+  geom_text(aes(label = round(rmean, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "31 Pitch Count")
+count_plot31
+
+count_plot312 <-  c10_c20_c30_c31filter %>% 
+  ggplot(aes(x=STATE, y=outs, fill=rpos)) +
+  geom_tile() +
+  geom_text(aes(label = round(rpos, 3))) +
+  scale_fill_gradient2(low = "grey10", high = crcblue, 
+                       mid = "white") +
+  labs(title = "31 Pitch Count")
+count_plot312
+grid.arrange(count_plot00 ,count_plot002, count_plot30 ,count_plot302, 
+             count_plot1_, count_plot12_, count_plot1_2, count_plot1_22,
+             count_plot312, count_plot31, nrow = 5)
+
+data2022 %>% filter(STATE == "111 0") %>% filter(max(c30, c31) == 1) -> df
