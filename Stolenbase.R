9eef51bd0e43d40fd98dba774e861438063ce6d6
jasonh7702 <127449610+jasonh7702@users.noreply.github.com>
2024-06-18 18:35:37 -0400

    Update Stolenbase.R
    2018-2023 stolen base returns

--- Stolenbase.R
+++ Stolenbase.R
@@ -191,3 +191,213 @@ R18_2023 <- RUNS_out_2023["100", "2 outs"]
 expected_return_SB_2023 <- (p18_25_2023 * R25_2023 + p18_19_2023 * R19_2023) - R18_2023
 expected_return_SB_2023
 
+#adding the functionalized state code 
+addstatevar <- function(df){
+  df %>% 
+    mutate(RUNS =  AWAY_SCORE_CT + HOME_SCORE_CT,
+           HALF.INNING = paste(GAME_ID, INN_CT, BAT_HOME_ID),
+           RUNS.SCORED = (BAT_DEST_ID > 3) + (RUN1_DEST_ID > 3) +
+             (RUN2_DEST_ID > 3) + (RUN3_DEST_ID > 3)) -> df
+  df %>% 
+    group_by(HALF.INNING) %>% 
+    summarize(Outs.Inning = sum(EVENT_OUTS_CT),
+              Runs.Inning = sum(RUNS.SCORED),
+              Runs.Start = first(RUNS),
+              MAX.RUNS = Runs.Inning + Runs.Start) -> half_innings
+  
+  df %>%  inner_join(half_innings, by = "HALF.INNING") %>% 
+    mutate(RUNS.ROI = MAX.RUNS - RUNS) -> df
+  
+  df %>% 
+    mutate(BASES = paste(ifelse(BASE1_RUN_ID > '', 1, 0),
+                         ifelse(BASE2_RUN_ID > '', 1, 0),
+                         ifelse(BASE3_RUN_ID > '', 1, 0), sep = ""),
+           STATE = paste(BASES, OUTS_CT)) -> df
+  
+  df %>% 
+    mutate(NRUNNER1 = 
+             as.numeric(RUN1_DEST_ID == 1 | BAT_DEST_ID == 1),
+           NRUNNER2 =
+             as.numeric(RUN1_DEST_ID == 2 | RUN2_DEST_ID == 2 |
+                          BAT_DEST_ID == 2),
+           NRUNNER3 =
+             as.numeric(RUN1_DEST_ID == 3 | RUN2_DEST_ID == 3 |
+                          RUN3_DEST_ID == 3 | BAT_DEST_ID == 3),
+           NOUTS = OUTS_CT + EVENT_OUTS_CT,
+           NEW.BASES = paste(NRUNNER1, NRUNNER2,
+                             NRUNNER3, sep = ""),
+           NEW.STATE = paste(NEW.BASES, NOUTS)) -> df
+  
+  df %>% 
+    filter(STATE != NEW.STATE | (RUNS.SCORED > 0)) -> df
+  return(df)
+}
+
+#Starting with 2018 
+data2018 <- addstatevar(data2018)
+
+data2018 %>% 
+  filter(Outs.Inning == 3) -> data2018C 
+
+data2018C %>% 
+  group_by(STATE) %>% 
+  summarize(Mean = mean(RUNS.ROI)) %>% 
+  mutate(Outs = substr(STATE, 5, 5)) %>% 
+  arrange(Outs) -> RUNS2018
+
+RUNS_out_2018 <- matrix(round(RUNS2018$Mean, 2),8, 3)
+dimnames(RUNS_out_2018)[[2]] <- c("0 outs", "1 out", "2 outs")
+dimnames(RUNS_out_2018)[[1]] <- c("000", "001", "010", "011"
+                             ,"100", "101", "110", "111")
+filtered_data2018 <- data2018 %>% 
+  filter(EVENT_CD %in% c(4, 6))
+
+filtered_data2018 %>% 
+  count(NEW.STATE) %>% 
+  mutate(Percentage = n / sum(n) * 100)
+
+s0_2018 <- filtered_data2018 %>% filter(STATE == "100 2")
+
+transition_probs_2018 <- s0_2018 %>% 
+  count(NEW.STATE) %>% 
+  mutate(Percentage = n / sum(n) * 100)
+
+p18_25_2018 <- transition_probs_2018 %>% filter(NEW.STATE == "000 3") %>% pull(Percentage) / 100
+p18_19_2018 <- transition_probs_2018 %>% filter(NEW.STATE == "010 2") %>% pull(Percentage) / 100
+
+R25_2022 <- RUNS_out_2018["000", "0 outs"]
+R19_2022 <- RUNS_out_2018["010", "2 outs"]
+R18_2022 <- RUNS_out_2018["100", "2 outs"]
+
+expected_return_SB_2018 <- (p18_25_2022 * R25_2022 + p18_19_2022 * R19_2022) - R18_2022
+expected_return_SB_2018
+
+#Expected return calculations for 2019 
+data2019 <- addstatevar(data2019)
+
+data2019 %>% 
+  filter(Outs.Inning == 3) -> data2019C 
+
+data2019C %>% 
+  group_by(STATE) %>% 
+  summarize(Mean = mean(RUNS.ROI)) %>% 
+  mutate(Outs = substr(STATE, 5, 5)) %>% 
+  arrange(Outs) -> RUNS2019
+
+RUNS_out_2019 <- matrix(round(RUNS2019$Mean, 2), 8, 3)
+dimnames(RUNS_out_2019)[[2]] <- c("0 outs", "1 out", "2 outs")
+dimnames(RUNS_out_2019)[[1]] <- c("000", "001", "010", "011", "100", "101", "110", "111")
+
+filtered_data2019 <- data2019 %>% 
+  filter(EVENT_CD %in% c(4, 6))
+
+s0_2019 <- filtered_data2019 %>% filter(STATE == "100 2")
+
+transition_probs_2019 <- s0_2019 %>% 
+  count(NEW.STATE) %>% 
+  mutate(Percentage = n / sum(n) * 100)
+
+p18_25_2019 <- transition_probs_2019 %>% filter(NEW.STATE == "000 3") %>% pull(Percentage) / 100
+p18_19_2019 <- transition_probs_2019 %>% filter(NEW.STATE == "010 2") %>% pull(Percentage) / 100
+
+R25_2019 <- RUNS_out_2019["000", "0 outs"]
+R19_2019 <- RUNS_out_2019["010", "2 outs"]
+R18_2019 <- RUNS_out_2019["100", "2 outs"]
+
+expected_return_SB_2019 <- (p18_25_2019 * R25_2019 + p18_19_2019 * R19_2019) - R18_2019
+expected_return_SB_2019
+
+#Expected return for 2020
+data2020 <- addstatevar(data2020)
+
+data2020 %>% 
+  filter(Outs.Inning == 3) -> data2020C 
+
+data2020C %>% 
+  group_by(STATE) %>% 
+  summarize(Mean = mean(RUNS.ROI)) %>% 
+  mutate(Outs = substr(STATE, 5, 5)) %>% 
+  arrange(Outs) -> RUNS2020
+
+RUNS_out_2020 <- matrix(round(RUNS2020$Mean, 2), 8, 3)
+dimnames(RUNS_out_2020)[[2]] <- c("0 outs", "1 out", "2 outs")
+dimnames(RUNS_out_2020)[[1]] <- c("000", "001", "010", "011", "100", "101", "110", "111")
+
+filtered_data2020 <- data2020 %>% 
+  filter(EVENT_CD %in% c(4, 6))
+
+s0_2020 <- filtered_data2020 %>% filter(STATE == "100 2")
+
+transition_probs_2020 <- s0_2020 %>% 
+  count(NEW.STATE) %>% 
+  mutate(Percentage = n / sum(n) * 100)
+
+p18_25_2020 <- transition_probs_2020 %>% filter(NEW.STATE == "000 3") %>% pull(Percentage) / 100
+p18_19_2020 <- transition_probs_2020 %>% filter(NEW.STATE == "010 2") %>% pull(Percentage) / 100
+
+R25_2020 <- RUNS_out_2020["000", "0 outs"]
+R19_2020 <- RUNS_out_2020["010", "2 outs"]
+R18_2020 <- RUNS_out_2020["100", "2 outs"]
+
+expected_return_SB_2020 <- (p18_25_2020 * R25_2020 + p18_19_2020 * R19_2020) - R18_2020
+expected_return_SB_2020
+
+#Expected return for 2021
+data2021 <- addstatevar(data2021)
+
+data2021 %>% 
+  filter(Outs.Inning == 3) -> data2021C 
+
+data2021C %>% 
+  group_by(STATE) %>% 
+  summarize(Mean = mean(RUNS.ROI)) %>% 
+  mutate(Outs = substr(STATE, 5, 5)) %>% 
+  arrange(Outs) -> RUNS2021
+
+RUNS_out_2021 <- matrix(round(RUNS2021$Mean, 2), 8, 3)
+dimnames(RUNS_out_2021)[[2]] <- c("0 outs", "1 out", "2 outs")
+dimnames(RUNS_out_2021)[[1]] <- c("000", "001", "010", "011", "100", "101", "110", "111")
+
+filtered_data2021 <- data2021 %>% 
+  filter(EVENT_CD %in% c(4, 6))
+
+s0_2021 <- filtered_data2021 %>% filter(STATE == "100 2")
+
+transition_probs_2021 <- s0_2021 %>% 
+  count(NEW.STATE) %>% 
+  mutate(Percentage = n / sum(n) * 100)
+
+p18_25_2021 <- transition_probs_2021 %>% filter(NEW.STATE == "000 3") %>% pull(Percentage) / 100
+p18_19_2021 <- transition_probs_2021 %>% filter(NEW.STATE == "010 2") %>% pull(Percentage) / 100
+
+R25_2021 <- RUNS_out_2021["000", "0 outs"]
+R19_2021 <- RUNS_out_2021["010", "2 outs"]
+R18_2021 <- RUNS_out_2021["100", "2 outs"]
+
+expected_return_SB_2021 <- (p18_25_2021 * R25_2021 + p18_19_2021 * R19_2021) - R18_2021
+expected_return_SB_2021
+
+#Creating a data frame for the expected return on stolen bases
+expected_returns_df <- data.frame(
+  Year = 2018:2023,
+  Expected_Return_SB = c(
+    expected_return_SB_2018, 
+    expected_return_SB_2019, 
+    expected_return_SB_2020, 
+    expected_return_SB_2021, 
+    expected_return_SB_2022, 
+    expected_return_SB_2023
+  )
+)
+
+print(expected_returns_df)
+
+library(ggplot2)
+#creating a plot to visualize the returns
+ggplot(expected_returns_df, aes(x = Year, y = Expected_Return_SB)) +
+  geom_line(color = "blue") +
+  geom_point(color = "red") +
+  labs(title = "Expected Return of a Stolen Base Attempt (2018-2023)",
+       x = "Year",
+       y = "Expected Return SB") +
+  theme_minimal()
