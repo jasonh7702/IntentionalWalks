cd49d13e8f6f05f2fd2ce01a33df1de8ee21b49e
jasonaosborne <jaosborn@ncsu.edu>
2024-07-10 11:42:21 -0400

    creates and orders a vector of state names
    need this to populate transition probability vectors and compute returns on SBAs

---
+++ define-state-names.r
@@ -0,0 +1,9 @@
+load("/home/jaosborn/research/sports/mlb/parkfactors/datafiles/retrosheet/data2023.Rdata")
+source("addstatevar.R")
+data2023.states <- addstatevar(data2023)
+data2023.states %>% select(NEW.STATE) %>% table %>% names -> state.names
+oindx <- c(1,13,7,4,19,16,10,22,1+c(1,13,7,4,19,16,10,22),2+c(1,13,7,4,19,16,10,22),25)
+state.names <- state.names[oindx]
+
+# as a transition probability matrix
+# t(matrix(state.names,nrow=8)) 
