52b8712e74cedd5e68aaa71beed4e7a790065c4a
jasonh7702 <127449610+jasonh7702@users.noreply.github.com>
2024-07-12 02:49:03 -0400

    1 000 1

--- Stolenbase.R
+++ Stolenbase.R
@@ -58,10 +58,64 @@ data2021.states <- addstatevar(data2021)
 data2022.states <- addstatevar(data2022)
 data2023.states <- addstatevar(data2023)
 
-
-source("RunExp-fcn.r")
-Rmtx.2023 <- runExpMtx(data2023)
-as.vector(t(Rmtx.2023))
+#Run expectancy matrix
+runExpMtx <- function(df){
+  df %>% 
+    mutate(RUNS =  AWAY_SCORE_CT + HOME_SCORE_CT,
+           HALF.INNING = paste(GAME_ID, INN_CT, BAT_HOME_ID),
+           RUNS.SCORED = (BAT_DEST_ID > 3) + (RUN1_DEST_ID > 3) +
+             (RUN2_DEST_ID > 3) + (RUN3_DEST_ID > 3)) -> df
+  df %>% 
+    group_by(HALF.INNING) %>% 
+    summarize(Outs.Inning = sum(EVENT_OUTS_CT),
+              Runs.Inning = sum(RUNS.SCORED),
+              Runs.Start = first(RUNS),
+              MAX.RUNS = Runs.Inning + Runs.Start) -> half_innings
+  
+  df %>%  inner_join(half_innings, by = "HALF.INNING") %>% 
+    mutate(RUNS.ROI = MAX.RUNS - RUNS) -> df
+  
+  df %>% 
+    mutate(BASES = paste(ifelse(BASE1_RUN_ID > '', 1, 0),
+                         ifelse(BASE2_RUN_ID > '', 1, 0),
+                         ifelse(BASE3_RUN_ID > '', 1, 0), sep = ""),
+           STATE = paste(BASES, OUTS_CT)) -> df
+  
+  df %>% 
+    mutate(NRUNNER1 = 
+             as.numeric(RUN1_DEST_ID == 1 | BAT_DEST_ID == 1),
+           NRUNNER2 =
+             as.numeric(RUN1_DEST_ID == 2 | RUN2_DEST_ID == 2 |
+                          BAT_DEST_ID == 2),
+           NRUNNER3 =
+             as.numeric(RUN1_DEST_ID == 3 | RUN2_DEST_ID == 3 |
+                          RUN3_DEST_ID == 3 | BAT_DEST_ID == 3),
+           NOUTS = OUTS_CT + EVENT_OUTS_CT,
+           NEW.BASES = paste(NRUNNER1, NRUNNER2,
+                             NRUNNER3, sep = ""),
+           NEW.STATE = paste(NEW.BASES, NOUTS)) -> df
+  
+  df %>% 
+    filter(STATE != NEW.STATE | (RUNS.SCORED > 0)) -> df
+  
+  # last step is to restrict attention to complete innings
+  df %>% 
+    filter(Outs.Inning == 3) -> dfC 
+  
+  dfC %>% 
+    group_by(STATE) %>% 
+    summarize(Mean = mean(RUNS.ROI)) %>% 
+    mutate(Outs = substr(STATE, 5, 5)) %>% 
+    arrange(Outs) -> RUNSdf
+  
+  #RUNS_out <- matrix(round(RUNSdf$Mean, 2),8, 3)
+  RUNS_out <- matrix(RUNSdf$Mean,8, 3)
+  dimnames(RUNS_out)[[2]] <- c("0 outs", "1 out", "2 outs")
+  dimnames(RUNS_out)[[1]] <- c("000", "001", "010", "011"
+                               ,"100", "101", "110", "111")
+  RUNS_out <- RUNS_out[c(1,5,3,2,7,6,4,8),]
+  return(RUNS_out)
+}
 
 
 
@@ -240,11 +294,6 @@ sba.return.1002 <- function(df)
 
 
 
-data2023.states <- addstatevar(data2023)
-data2023.states %>% select(NEW.STATE) %>% table %>% names -> state.names
-oindx <- c(1,13,7,4,19,16,10,22,1+c(1,13,7,4,19,16,10,22),2+c(1,13,7,4,19,16,10,22),25)
-state.names <- state.names[oindx]
-
 returns.mtx <- c(season=integer(),state=character(),runExp = double(), runExp.sba=double(),sbaReturn = double())
 
 for(season in 2018:2023){
@@ -259,3 +308,36 @@ returns.df <- data.frame(returns.mtx)
 row.names(returns.df) <- NULL
 print(returns.df)
 
+
+sba.return.1001 <- function(df)
+{
+  Rmtx <- runExpMtx(df)
+  df.states <- addstatevar(df)
+  df.states %>% filter(EVENT_CD %in% c(4, 6)) -> df.states.sba
+  
+  # Mutate NEW.STATE into a new factor called NEW.STATE.FACTOR
+  df.states.sba %>% mutate(NEW.STATE.FACTOR = factor(NEW.STATE, levels=state.names)) -> df.states.sba
+  
+  # Obtain relative frequencies (including 0s) for all 25 states
+  df.states.sba %>% filter(STATE == "100 1") -> df.states.sba.1001
+  df.states.sba.1001 %>% select(NEW.STATE.FACTOR) %>% table %>% prop.table -> tpm.1001 # Transition probability matrix as vector
+  
+  Rmtx.1001 <- tpm.1001 %*% c(as.vector(Rmtx), 0) + 1 * tpm.1001[17]
+  sbaReturn.1001 <- Rmtx.1001 - Rmtx[2, 2]
+  
+  return(list(state = "100 1", tpm = tpm.1001, runExp = Rmtx[2, 2], runExp.sba = Rmtx.1001, sba.return = sbaReturn.1001))
+}
+
+returns.mtx <- c(season=integer(),state=character(),runExp = double(), runExp.sba=double(),sbaReturn = double())
+
+for(season in 2018:2023){
+  fn <- paste0("data",season,".RData")
+  load(fn)
+  df <- get(paste0("data",season))
+  out.list <- sba.return.1001(df)
+  datarow <- c(season=season,state=out.list$state,runExp = out.list$runExp, runExp.sba=out.list$runExp.sba,sbaReturn = out.list$sba.return)
+  returns.mtx <- rbind(returns.mtx,datarow)
+}
+returns.df <- data.frame(returns.mtx)
+row.names(returns.df) <- NULL
+print(returns.df)
